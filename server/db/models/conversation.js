8ff772461e82659a1de21c1eb445fad60947b4ca
Jesus Salazar <jase036@gmail.com>
2022-05-03 16:49:03 +0000

    method change revert (not required by ticket)

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -1,24 +1,25 @@
 const { Op } = require("sequelize");
 const db = require("../db");
-
+const Message = require("./message");
 
 const Conversation = db.define("conversation", {});
 
-// find conversations given an array of user Ids
+// find conversation given two user Ids
 
-Conversation.findConversations = async function (userArr) {
-  const conversations = await Conversation.findAll({
-    include: [{
-      model: User,
-      through: { where: {
-        userId: { [Op.in]: userArr }
-      }}
-    }],
-    
+Conversation.findConversation = async function (user1Id, user2Id) {
+  const conversation = await Conversation.findOne({
+    where: {
+      user1Id: {
+        [Op.or]: [user1Id, user2Id]
+      },
+      user2Id: {
+        [Op.or]: [user1Id, user2Id]
+      }
+    }
   });
 
   // return conversation or null if it doesn't exist
-  return conversations;
+  return conversation;
 };
 
 module.exports = Conversation;
